162f252c0881b352ef0a8e96ab63752ab3ba8182
渡辺大稀 <i.likesusi.d@gmail.com>
2019-01-06 23:58:37 +0900

    modify brainfuck.scala

--- parser/src/main/scala/example/brainfuck.scala
+++ parser/src/main/scala/example/brainfuck.scala
@@ -12,53 +12,81 @@ object Parser extends Brainfuck {
         if (args.length == 2) {
 
         } else {
-            val src1 = """++++++++++++++++++++++++++++++++
-                        ++++++++++++++++++++++++++++++++
-                        ++++++++++++++++++++++++++++++++
-                        ++++++++.
-                        +++++++.
-                        --------.
-                        --."""
-            val src2 = """++++++++++[>++++++++++<-]>
-                         ++++.+++++++.--------.--."""
-            println(parseAll(program, src2))
+            val src1 = """
+                ++++++++++++++++++++++++++++++++
+                ++++++++++++++++++++++++++++++++
+                ++++++++++++++++++++++++++++++++
+                ++++++++.
+                +++++++.
+                --------.
+                --.
+            """
+            val src2 = """
+                ++++++++++[>++++++++++<-]>
+                ++++.+++++++.--------.--.
+            """
+            val src3 = """
+                >+++++++++[<++++++++>-]<.>+++++++[<++++>-]<+.+++++++..+++.[-]>++++++++[<++
+                ++>-]<.>+++++++++++[<+++++>-]<.>++++++++[<+++>-]<.+++.------.--------.[-]>
+                ++++++++[<++++>-]<+.[-]++++++++++.
+            """
+
+            val src4 = "-"
+            // println(parseAll(program, src3))
+            apply(src3)
         }
     }
 }
+
 
 object Runtime {
     private val memories = new Array[Int](1024)
     private var index = 0
     private val buffer = new StringBuilder
 
-    def eval(opt: String): Unit = opt match {
-        case ">" => index += 1
-        case "<" => index -= 1
-        case "+" => memories(index) += 1
-        case "-" => memories(index) -= 1
-        case "." => memories(index) = scala.io.StdIn.readInt
-        case "," => buffer.append(memories(index).toChar)
-        case _   => ()
+    abstract class RuntimeObject{
+        def evaluate: Unit
+    }
+    class Prog(procs: List[Proc]) extends RuntimeObject {
+        override def evaluate: Unit = {
+            println("*********** Evaluating Brainf*ck START **********")
+            procs.map(_.evaluate)
+            println("*********** Evaluating Brainf*ck END   **********")
+        }
     }
 
-    def value: Int = memories(index)
+    abstract class Proc extends RuntimeObject
+    class Opt(tok: String) extends Proc {
+        override def evaluate: Unit = tok match {
+            case ">" => index += 1
+            case "<" => index -= 1
+            case "+" => memories(index) += 1
+            case "-" => memories(index) -= 1
+            case "." => print(memories(index).toChar)
+            case "," => memories(index) = scala.io.StdIn.readInt
+            case _   => ()
+        }
+
+    }
+    class Loop(toks: List[Proc]) extends Proc {
+        override def evaluate: Unit = while(memories(index) != 0) toks.map(_.evaluate)
+    }
 }
+
 /**
  * Language Brainf*ck
- * 入出力ストリームは外部から設定する。デフォルトは標準入出力
- * フィールドはバイト型の配列と配列の添字
- * 基本的に8つの命令はアクション`() => Unit`を実行する。
- * ループ
- * 　1. ポインタがさす領域の値が0かチェック
- * 　2. 0でなければ、任意の手続きを実行し、1に戻る。
- * 　3. 0であれば、ループを抜ける。
- * @type {Array}
  */
 class Brainfuck extends RegexParsers {
 
-    // プログラムは1つ以上の命令の繰り返しである。
-    def program: Parser[Any] = rep(opt)
+    def apply(input: String): Unit = parseAll(program, input) match {
+        case Success(result, _) => result.evaluate
+        case failure: NoSuccess => scala.sys.error(failure.msg)
+    }
 
-    // 命令はポインタ演算’（>, <）、数値演算（+, -）、標準入出力（ピリオドとコンマ）またはループ記述である。
-    def opt: Parser[Any] = """[><+-.,]""".r | "[" ~> program <~ "]"
+    def program  : Parser[Runtime.Prog] = rep(procedure) ^^ (new Runtime.Prog(_))
+
+    def procedure: Parser[Runtime.Proc] = operator | loop
+
+    def operator : Parser[Runtime.Opt] = """[><+-.,]""".r ^^ (new Runtime.Opt(_))
+    def loop     : Parser[Runtime.Loop] = "[" ~> rep(procedure) <~ "]" ^^ (new Runtime.Loop(_))
 }
